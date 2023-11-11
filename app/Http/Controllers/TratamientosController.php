686a0939e68e80b7ad46062f0c772bd672647e5a
miguel <miguel.poveda@akold.com>
2023-11-11 13:14:16 -0500

    Tratamiento
    lista de tratamiento, detalle, modal

--- app/Http/Controllers/TratamientosController.php
+++ app/Http/Controllers/TratamientosController.php
@@ -10,4 +10,14 @@ class TratamientosController extends Controller
     public function misTratamientos() {
         return view('tratamientos.index');
     }
+
+    // Return view lista de tratamientos
+    public function tratamientos() {
+        return view('tratamientos.lista_tratamientos');
+    }
+
+    // Return view detalle de tratamiento
+    function detalleTratamiento() {
+        return view('tratamientos.detalle');
+    }
 }
--- resources/views/tratamientos/index.blade.php
+++ resources/views/tratamientos/index.blade.php
@@ -64,6 +64,7 @@ Mi Veris - Citas - Mis tratamientos
                     <div class="d-flex justify-content-center">
                         <div class="col-12 col-md-10 col-lg-8">
                             <div class="row g-3">
+                                <!-- items -->
                                 <div class="col-12 col-md-6">
                                     <div class="card">
                                         <div class="card-body p-2">
@@ -84,12 +85,13 @@ Mi Veris - Citas - Mis tratamientos
                                         </div>
                                     </div>
                                 </div>
+                                <!-- items -->
                                 <div class="col-12 col-md-6">
                                     <div class="card">
                                         <div class="card-body p-2">
                                             <div class="row gx-0 justify-content-between align-items-center mb-2">
                                                 <div class="col-9">
-                                                    <h6 class="card-title text-primary-veris mb-0">Traumatología</h6>
+                                                    <h6 class="card-title text-primary-veris mb-0">Otorrinolaringología</h6>
                                                     <p class="fw-bold fs--2 mb-0">María Yanina Donoso Samaniego</p>
                                                     <p class="card-text fs--2 mb-0">Dr(a): Magdalena Caroline Hernandez...</p>
                                                     <p class="fw-normal fs--2 mb-0">Tratamiento enviado: <b class="fecha-enviado fw-normal text-primary-veris">AGO 09, 2022</b></p>
@@ -98,18 +100,19 @@ Mi Veris - Citas - Mis tratamientos
                                                     <div id="chart-progress" data-porcentaje="80" data-color="success"><i class="bi bi-check2 position-absolute top-25 start-40 success"></i></div>
                                                 </div>
                                                 <div class="d-flex justify-content-end align-items-center">
-                                                    <a href="#" class="btn btn-sm btn-primary-veris">Empezar</a>
+                                                    <a href="{{route('tratamientos.lista')}}" class="btn btn-sm btn-primary-veris">Continuar</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- items -->
                                 <div class="col-12 col-md-6">
                                     <div class="card">
                                         <div class="card-body p-2">
                                             <div class="row gx-0 justify-content-between align-items-center mb-2">
                                                 <div class="col-9">
-                                                    <h6 class="card-title text-primary-veris mb-0">Traumatología</h6>
+                                                    <h6 class="card-title text-primary-veris mb-0">Dermatología</h6>
                                                     <p class="fw-bold fs--2 mb-0">María Yanina Donoso Samaniego</p>
                                                     <p class="card-text fs--2 mb-0">Dr(a): Magdalena Caroline Hernandez...</p>
                                                     <p class="fw-normal fs--2 mb-0">Tratamiento enviado: <b class="fecha-enviado fw-normal text-primary-veris">AGO 09, 2022</b></p>
@@ -118,30 +121,119 @@ Mi Veris - Citas - Mis tratamientos
                                                     <div id="chart-progress" data-porcentaje="20" data-color="success"><i class="bi bi-check2 position-absolute top-25 start-40 success"></i></div>
                                                 </div>
                                                 <div class="d-flex justify-content-end align-items-center">
-                                                    <a href="#" class="btn btn-sm btn-primary-veris">Empezar</a>
+                                                    <a href="{{route('tratamientos.lista')}}" class="btn btn-sm btn-primary-veris">Continuar</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="pills-realizados" role="tabpanel" aria-labelledby="pills-realizados-tab" tabindex="0">
-                    <div class="col-auto bg-white p-2">
-                        <button class="btn btn-sm btn-outline-primary-veris" type="button" data-bs-toggle="offcanvas" data-bs-target="#filtroTratamientos" aria-controls="filtroTratamientos"><i class="bi bi-sliders me-3"></i> Maria Donoso</button>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <div class="col-12 col-md-10 col-lg-8">
-                            
-                            <div class="row">
-                                <div class="col-12 d-flex justify-content-center">
+                                <!-- Mensaje No tiene tratamiento -->
+                                <div class="col-12 d-flex justify-content-center d-none">
                                     <div class="card bg-transparent shadow-none">
                                         <div class="card-body">
                                             <div class="text-center">
                                                 <h5>No tienes tratamientos</h5>
                                                 <p>En esta sección podrás revisar tus tratamientos</p>
+                                                <img src="{{ asset('assets/img/svg/sin_tratamiento.svg') }}" class="img-fluid" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Mensaje END -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="pills-realizados" role="tabpanel" aria-labelledby="pills-realizados-tab" tabindex="0">
+                    <div class="col-auto bg-white p-2 mb-3">
+                        <button class="btn btn-sm btn-outline-primary-veris" type="button" data-bs-toggle="offcanvas" data-bs-target="#filtroTratamientos" aria-controls="filtroTratamientos"><i class="bi bi-sliders me-3"></i> Maria Donoso</button>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="col-12 col-md-10 col-lg-8">
+                            <div class="row g-3">
+                                <!-- items -->
+                                <div class="col-12 col-md-6">
+                                    <div class="card">
+                                        <div class="card-body position-relative p-3">
+                                            <div class="position-absolute end-0">
+                                                <img src="{{ asset('assets/img/svg/golden.svg') }}" class="pe-3" alt="golden">
+                                            </div>
+                                            <div class="text-center">
+                                                <div class="col-auto">
+                                                    <div id="chart-progress" data-porcentaje="10" data-color="success"><i class="bi bi-check2 position-absolute top-25 start-40 success"></i></div>
+                                                </div>
+                                                <h6 class="card-title mb-2">Traumatología</h6>
+                                            </div>
+                                            <div class="d-flex justify-content-between align-items-end">
+                                                <div>
+                                                    <p class="fw-bold fs--2 mb-0">¡Tratamiento terminado!</p>
+                                                    <p class="fw-normal fs--2 mb-0">María Yanina Donoso Samaniego</p>
+                                                    <p class="fw-light fs--2 mb-0">Terminado el: <b class="text-primary-veris fw-light fs--2" id="fechaTratamiento">SEP 09,2022</b></p>
+                                                </div>
+                                                <div>
+                                                    <a href="#" class="btn btn-sm btn-primary-veris">Ver todo</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="card">
+                                        <div class="card-body position-relative p-3">
+                                            <div class="position-absolute end-0">
+                                                <img src="{{ asset('assets/img/svg/golden.svg') }}" class="pe-3" alt="golden">
+                                            </div>
+                                            <div class="text-center">
+                                                <div class="col-auto">
+                                                    <div id="chart-progress" data-porcentaje="10" data-color="success"><i class="bi bi-check2 position-absolute top-25 start-40 success"></i></div>
+                                                </div>
+                                                <h6 class="card-title mb-2">Traumatología</h6>
+                                            </div>
+                                            <div class="d-flex justify-content-between align-items-end">
+                                                <div>
+                                                    <p class="fw-bold fs--2 mb-0">¡Tratamiento terminado!</p>
+                                                    <p class="fw-normal fs--2 mb-0">María Yanina Donoso Samaniego</p>
+                                                    <p class="fw-light fs--2 mb-0">Terminado el: <b class="text-primary-veris fw-light fs--2" id="fechaTratamiento">SEP 09,2022</b></p>
+                                                </div>
+                                                <div>
+                                                    <a href="#" class="btn btn-sm btn-primary-veris">Ver todo</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="card">
+                                        <div class="card-body position-relative p-3">
+                                            <div class="position-absolute end-0">
+                                                <img src="{{ asset('assets/img/svg/golden.svg') }}" class="pe-3" alt="golden">
+                                            </div>
+                                            <div class="text-center">
+                                                <div class="col-auto">
+                                                    <div id="chart-progress" data-porcentaje="10" data-color="success"><i class="bi bi-check2 position-absolute top-25 start-40 success"></i></div>
+                                                </div>
+                                                <h6 class="card-title mb-2">Traumatología</h6>
+                                            </div>
+                                            <div class="d-flex justify-content-between align-items-end">
+                                                <div>
+                                                    <p class="fw-bold fs--2 mb-0">¡Tratamiento terminado!</p>
+                                                    <p class="fw-normal fs--2 mb-0">María Yanina Donoso Samaniego</p>
+                                                    <p class="fw-light fs--2 mb-0">Terminado el: <b class="text-primary-veris fw-light fs--2" id="fechaTratamiento">SEP 09,2022</b></p>
+                                                </div>
+                                                <div>
+                                                    <a href="#" class="btn btn-sm btn-primary-veris">Ver todo</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Mensaje No tiene tratamiento -->
+                                <div class="col-12 d-flex justify-content-center d-none">
+                                    <div class="card bg-transparent shadow-none">
+                                        <div class="card-body">
+                                            <div class="text-center">
+                                                <h5>No tienes tratamientos realizados</h5>
+                                                <p>En esta sección podrás ver los tratamientos terminados</p>
                                                 <img src="{{ asset('assets/img/svg/sin_tratamiento.svg') }}" class="img-fluid" alt="">
                                             </div>
                                         </div>
