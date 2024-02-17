485bc8c4ccae5a4f9523cd1f1993da34a3b1249a
miguel <miguel.poveda@akold.com>
2024-02-17 16:06:56 -0500

    Ajuste de filtro posición del botón atras

--- resources/views/components/barraFiltro.blade.php
+++ resources/views/components/barraFiltro.blade.php
@@ -1,7 +1,7 @@
 {{-- Filtro para pendientes --}}
 <div class="col-auto bg-white p-2">
     <button class="btn btn-sm btn-outline-primary-veris ms-2 px-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#filtroTratamientos" aria-controls="filtroTratamientos" ><img src="{{asset('assets/img/svg/filtro.svg')}}" class="me-1" alt="filtro"> 
-        <p class="fs--1 line-height-16 mb-0" id="nombreFiltro"></p>
+        <p class="fs--1 line-height-16 fw-normal mb-0" id="nombreFiltro"></p>
     </button>
 </div>
 
