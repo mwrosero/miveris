f22c9fe70de1cb437fadc9347689b0081e49b80a
Michael Rosero <michael.rosero@akold.com>
2024-03-28 08:30:31 -0500

    scroll mobile
    ajuste

--- resources/views/template/sidebar2.blade.php
+++ resources/views/template/sidebar2.blade.php
@@ -33,8 +33,8 @@
                 <div data-i18n="Resultados">Resultados</div>
             </a>
         </li>
-        <li class="menu-item {{ Route::is('promociones.mis-promociones') || Route::is('promociones.*')  ? 'active' : '' }}">
-            <a href="{{route('promociones.mis-promociones')}}" class="menu-link fs--1 text-white">
+        <li class="menu-item {{ Route::is('promociones.promociones') || Route::is('promociones.*')  ? 'active' : '' }}">
+            <a href="{{route('promociones.promociones')}}" class="menu-link fs--1 text-white">
                 <div class="svg-container svg-promociones me-3"></div>
                 <div data-i18n="Promociones compradas">Promociones compradas</div>
             </a>
