3b0218473b8022c0e0d5b7b4c11faca21a6d33b8
Michael Rosero <michael.rosero@akold.com>
2025-03-06 20:51:43 -0500

    pasarela

--- resources/views/external/pasarela/datos_facturacion.blade.php
+++ resources/views/external/pasarela/datos_facturacion.blade.php
@@ -9,6 +9,7 @@ Veris - Datos de facturación
 @php
     $tokenCita = base64_encode(uniqid());
 @endphp
+
 <link rel="stylesheet" href="{{ asset('assets/css/theme-veris-app.css?v=1.0')}}">
 <script src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/js/veris-helper.js"></script>
 
