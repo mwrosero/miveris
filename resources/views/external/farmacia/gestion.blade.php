3eac790a9d2270f9063535b730c0f3db6bfbda9a
Michael Rosero <michael.rosero@akold.com>
2025-05-20 13:50:32 -0500

    Updates success modal title in picking process
    Changes the success modal title to more accurately reflect the action performed, focusing on the picking generation process.

--- resources/views/external/farmacia/gestion.blade.php
+++ resources/views/external/farmacia/gestion.blade.php
@@ -41,7 +41,7 @@
         <div class="modal-dialog modal-md modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header justify-content-center">
-                    <h5 class="modal-title" id="confirmacionTitle">Picking actualizado correctamente</h5>
+                    <h5 class="modal-title" id="confirmacionTitle">Se ha generado el picking correctamente.</h5>
                     {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
                 </div>
                 <div class="modal-body text-center" id="confirmacionBody">
