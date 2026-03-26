e9310906a59acc4e632cdee1b57569811d651bbc
Michael Rosero <michael.rosero@akold.com>
2026-03-26 14:40:34 -0500

    IA

--- resources/views/external/portal_cautivo/index_portal_cautivo.blade.php
+++ resources/views/external/portal_cautivo/index_portal_cautivo.blade.php
@@ -85,7 +85,7 @@ Veris - Portal Cautivo
 								            <img src="{{ asset('assets/external/bot/logo-vericita.png') }}" width="75px">
 								        	<h2 class="text-secundario-midnight-blue-00 mb-0">Comencemos con tu identificación.</h2>
 								        </div>
-								        <div class="bg-white m-3 rounded-3 mb-0 gap-2 row mx-0 p-4">
+								        <div class="bg-white m-3 rounded-3 mb-0 gap-2 row mx-0 p-4 swipe-up-entry">
 										  	<div class="col-md-12 mb-2">
 				                                <label for="tipoIdentificacion" class="form-label fw-medium fs--1">Elige tu documento *</label>
 				                                <select class="form-select fs--1 p-3" name="tipoIdentificacion" id="tipoIdentificacion" required>
@@ -124,6 +124,107 @@ Veris - Portal Cautivo
 							</div>
 						</div>
 					</div>
+
+					{{-- STEP 3 --}}
+					<div class="step step-3 d-none modal-dialog-scrollable">
+						<div class="modal-content">
+							<div class="msg-head bg-vris-light-grayish-blue-3">
+								<div class="btn-back settings-tray rounded d-flex justify-content-between align-items-center gap-2">
+									<div type="button" class="d-flex justify-content-between align-items-center gap-2 fw-medium">
+										<i class="fa-solid fa-arrow-left"></i>
+										Atrás
+									</div>
+									<img class="my-2" height="40px" src="{{ asset('assets/img/veris/logo-veris-2025.svg')}}">
+								</div>
+							</div>
+							<div class="modal-body">
+								<div class="h-100 d-flex flex-column justify-content-between">
+									<div class="d-flex flex-column align-items-start px-3 mt-auto">
+								        <div class="d-flex justify-content-between align-items-center gap-2 mb-3">
+								            <img src="{{ asset('assets/external/bot/logo-vericita.png') }}" width="75px">
+								            <div>
+									        	<h2 class="text-secundario-midnight-blue-00 mb-0">Hola, <span class="namePaciente">Michael</span>.</h2>
+									        	<p class="fs--16 line-height-20 text-secundario-midnight-blue-00 mb-0">Selecciona tu cita.</p>
+									        </div>
+								        </div>
+								        <div class="w-100 swipe-up-entry" id="lista-citas">
+									        <div type="button" class="bg-white m-3 rounded-3 mb-0 gap-2 row mx-0 py-3 px-2 w-100 shadow-sm item-cita">
+											  	<div class="col-md-12 mb-2">
+					                                <p class="text-secundario-midnight-blue-00 fs--16 line-height-20 fw-normal mb-2">Tendrás una cita con:</p>
+					                                <div class="box-horario d-flex justify-content-between align-items-center gap-3">
+					                                	<div style="background: url(https://dikg1979lm6fy.cloudfront.net/fotosMedicos/0913067625.jpg) no-repeat top center;    background-size: cover; border-radius: 100%; border: 1px solid #BAB9BE; width: 80px; height: 80px;">
+					                                	</div>
+					                                	<div class="infoCita flex-grow-1">
+					                                		<p class="fs--16 line-height-20 mb-1 text-secundario-midnight-blue-00 fw-medium">Arianna Sofía Flores Alvarado</p>
+					                                		<p class="fs--14 line-height-18 mb-1 text-Silver-676767 fw-normal">Medicina General</p>
+					                                		<p class="fs--14 line-height-18 mb-1 text-veris-ai fw-medium">Hoy, 3:00 pm</p>
+					                                	</div>
+					                                </div>
+					                            </div>
+											</div>
+											<div type="button" class="bg-white m-3 rounded-3 mb-0 gap-2 row mx-0 py-3 px-2 w-100 shadow-sm item-cita">
+											  	<div class="col-md-12 mb-2">
+					                                <p class="text-secundario-midnight-blue-00 fs--16 line-height-20 fw-normal mb-2">Tendrás una cita con:</p>
+					                                <div class="box-horario d-flex justify-content-between align-items-center gap-3">
+					                                	<div style="background: url(https://dikg1979lm6fy.cloudfront.net/fotosMedicos/0913067625.jpg) no-repeat top center;    background-size: cover; border-radius: 100%; border: 1px solid #BAB9BE; width: 80px; height: 80px;">
+					                                	</div>
+					                                	<div class="infoCita flex-grow-1">
+					                                		<p class="fs--16 line-height-20 mb-1 text-secundario-midnight-blue-00 fw-medium">Arianna Sofía Flores Alvarado</p>
+					                                		<p class="fs--14 line-height-18 mb-1 text-Silver-676767 fw-normal">Medicina General</p>
+					                                		<p class="fs--14 line-height-18 mb-1 text-veris-ai fw-medium">Hoy, 3:00 pm</p>
+					                                	</div>
+					                                </div>
+					                            </div>
+											</div>
+										</div>
+									    <div class="footer-action w-100">
+									        <button class="btn btn-primary-veris rounded-3 btn-continue w-100 mt-4 mb-1 py-4 fs--20 line-height-24 fw-medium" disabled id="btn-elegir-cita">Continuar</button>
+									        <div type="button" id="btn-datos-erroneos" class="text-center py-3 text-veris-ai fw-medium fs--18 line-height-24">No soy esa persona</div>
+									    </div>
+								    </div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					{{-- STEP 4 --}}
+					<div class="step step-4 d-none modal-dialog-scrollable">
+						<div class="modal-content">
+							<div class="msg-head bg-vris-light-grayish-blue-3">
+								<div class="btn-back settings-tray rounded d-flex justify-content-between align-items-center gap-2">
+									<div type="button" class="d-flex justify-content-between align-items-center gap-2 fw-medium">
+										<i class="fa-solid fa-arrow-left"></i>
+										Atrás
+									</div>
+									<img class="my-2" height="40px" src="{{ asset('assets/img/veris/logo-veris-2025.svg')}}">
+								</div>
+							</div>
+							<div class="modal-body">
+								<div class="h-100 d-flex flex-column justify-content-between">
+									<div class="d-flex flex-column align-items-start px-3 mt-auto">
+								        <div class="d-flex justify-content-between align-items-center gap-2 mb-3">
+								            <img src="{{ asset('assets/external/bot/logo-vericita.png') }}" width="75px">
+								            <div>
+								        		<h2 class="text-secundario-midnight-blue-00 mb-0">Ahora sí, comencemos.</h2>
+								        		<p class="fs--16 line-height-20 text-secundario-midnight-blue-00 mb-0">Te tomará solo unos minutos.</p>
+								        	</div>
+								        </div>
+								        <div class="bg-white m-3 rounded-3 mb-0 gap-2 row mx-0 py-3 px-2 w-100 shadow-sm swipe-up-entry">
+										  	<div class="col-md-12 mb-2">
+				                                {{-- <label class="form-label fw-medium fs--1">¿Cuál es el motivo de tu visita?</label> --}}
+				                                <p class="text-Secundario-Midnight-blue-Tint-20s fw-normal fs--18 line-height-24">¿Cuál es el motivo de tu visita?</p>
+				                            </div>
+				                        </div>
+									    <div class="footer-action w-100">
+									        <button class="btn btn-primary-veris rounded-3 btn-continue w-100 mt-4 mb-1 py-4 fs--20 line-height-24 fw-medium" disabled id="btn-elegir-cita">Continuar</button>
+									        <div type="button" id="btn-datos-erroneos" class="text-center py-3 text-veris-ai fw-medium fs--18 line-height-24">No soy esa persona</div>
+									    </div>
+								    </div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 			<!-- chatbox -->
@@ -169,14 +270,44 @@ Veris - Portal Cautivo
             }
         });
 
+        $('body').on('click', '#btn-validar', function(){
+			$('.step').addClass('d-none');
+			$('.step-3').removeClass('d-none');
+			//Realizar en el ws para obtener citas
+			$('.item-cita').removeClass('item-cita-selected');
+			$('#btn-elegir-cita').attr('disabled', true);
+			currentStep++;
+		})
+
+		$('body').on('click', '#btn-elegir-cita', function(){
+			let detalleCita = [];
+			$('.step').addClass('d-none');
+			$('.step-4').removeClass('d-none');
+			currentStep++;
+		})
+
         $('body').on('click', '.btn-back', function(){
         	switch(currentStep){
         		case 2:
-        			$('.step').addClass('d-none');
-					$('.step-1').removeClass('d-none');
+        		case 3:
+        		case 4:
 					currentStep--;
+        			$('.step').addClass('d-none');
+					$(`.step-${currentStep}`).removeClass('d-none');
         		break;
         	}
+        })
+
+        $('body').on('click', '#btn-datos-erroneos', function(){
+        	$('.step').addClass('d-none');
+			$('.step-2').removeClass('d-none');
+        	currentStep--;
+        })
+
+        $('body').on('click', '.item-cita', function(){
+        	$('.item-cita').removeClass('item-cita-selected');
+        	$(this).addClass('item-cita-selected');
+        	$('#btn-elegir-cita').attr('disabled', false);
         })
 		
 	});
